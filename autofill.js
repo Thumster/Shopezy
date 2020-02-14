0c770b9ea4993c61b3c30420dffd0e8bd5616e7c
JianYiee <hew1521@hotmail.com>
2020-02-15 01:31:19 +0800

    "Added button to open CASE and autofill"

---
+++ autofill.js
@@ -0,0 +1,165 @@
+var els=document.getElementsByName("COMPLAINT_SALUTATION_ID");
+for (var i=0;i<els.length;i++) {
+els[i].value = 1;}
+
+var els=document.getElementsByName("COMPLAINT_FIRSTNAME");
+for (var i=0;i<els.length;i++) {
+els[i].value = "Hew";}
+
+var els=document.getElementsByName("COMPLAINT_LASTNAME");
+for (var i=0;i<els.length;i++) {
+els[i].value = "Jian Yiee";}
+
+var els=document.getElementsByName("COMPLAINT_NRIC");
+for (var i=0;i<els.length;i++) {
+els[i].value = "870E";}
+
+var els=document.getElementsByName("COMPLAINT_UNION");
+for (var i=0;i<els.length;i++) {
+els[i].value = "-1";}
+
+var els=document.getElementsByName("COMPLAINT_DOB_DAY");
+for (var i=0;i<els.length;i++) {
+els[i].value = "6";}
+
+var els=document.getElementsByName("COMPLAINT_DOB_MONTH");
+for (var i=0;i<els.length;i++) {
+els[i].value = "9";}
+
+var els=document.getElementsByName("COMPLAINT_DOB_YEAR");
+for (var i=0;i<els.length;i++) {
+els[i].value = "1996";}
+
+var els=document.getElementsByName("COMPLAINT_OCCUPATION_ID");
+for (var i=0;i<els.length;i++) {
+els[i].value = "3";}
+
+var els=document.getElementsByName("COMPLAINT_EDUCATION_ID");
+for (var i=0;i<els.length;i++) {
+els[i].value = "3";}
+
+var els=document.getElementsByName("COMPLAINT_CASE_MEMBER");
+for (var i=0;i<els.length;i++) {
+els[i].value = false;}
+
+var els=document.getElementsByName("COMPLAINT_CASE_MEMBERSHIP_ID");
+for (var i=0;i<els.length;i++) {
+els[i].value = "837103";}
+
+var els=document.getElementsByName("COMPLAINT_CASE_DAY");
+for (var i=0;i<els.length;i++) {
+els[i].value = "4";}
+
+var els=document.getElementsByName("COMPLAINT_CASE_MONTH");
+for (var i=0;i<els.length;i++) {
+els[i].value = "6";}
+
+var els=document.getElementsByName("COMPLAINT_CASE_YEAR");
+for (var i=0;i<els.length;i++) {
+els[i].value = "2015";}
+
+var els=document.getElementsByName("COMPLAINT_MEMBERSHIPTYPE_ID");
+for (var i=0;i<els.length;i++) {
+els[i].value = "3";}
+
+var els=document.getElementsByName("COMPLAINT_RACE");
+for (var i=0;i<els.length;i++) {
+els[i].value = "1";}
+
+var els=document.getElementsByName("COMPLAINT_GENDER_ID");
+for (var i=0;i<els.length;i++) {
+els[i].value = "1";}
+
+var els=document.getElementsByName("COMPLAINT_ADDRESS1");
+for (var i=0;i<els.length;i++) {
+els[i].value = "414A";}
+
+var els=document.getElementsByName("COMPLAINT_UNIT");
+for (var i=0;i<els.length;i++) {
+els[i].value = "#20-24";}
+
+var els=document.getElementsByName("COMPLAINT_EMAIL");
+for (var i=0;i<els.length;i++) {
+els[i].value = "hew1521@hotmail.com";}
+
+var els=document.getElementsByName("COMPLAINT_ADDRESS2");
+for (var i=0;i<els.length;i++) {
+els[i].value = "Fernvale Link";}
+
+var els=document.getElementsByName("COMPLAINT_POSTALCODE");
+for (var i=0;i<els.length;i++) {
+els[i].value = "791414";}
+
+var els=document.getElementsByName("COMPLAINT_PHONE_HOME");
+for (var i=0;i<els.length;i++) {
+els[i].value = "64874704";}
+
+var els=document.getElementsByName("COMPLAINT_PHONE_MOBILE");
+for (var i=0;i<els.length;i++) {
+els[i].value = "97255472";}
+
+//Taken from https://shopee.sg/inboxmobile
+
+var els=document.getElementsByName("COMPLAINT_VENDOR_NAME");
+for (var i=0;i<els.length;i++) {
+els[i].value = "inboxmobile";}
+
+var els=document.getElementsByName("COMPLAINT_VENDOR_ADDRESS1");
+for (var i=0;i<els.length;i++) {
+els[i].value = "";}
+
+var els=document.getElementsByName("COMPLAINT_VENDOR_UNIT");
+for (var i=0;i<els.length;i++) {
+els[i].value = "#B1-139 ";}
+
+var els=document.getElementsByName("COMPLAINT_VENDOR_ADDRESS2");
+for (var i=0;i<els.length;i++) {
+els[i].value = "1 NorthPoint Drive";}
+
+var els=document.getElementsByName("COMPLAINT_VENDOR_POSTALCODE");
+for (var i=0;i<els.length;i++) {
+els[i].value = "768019";}
+
+var els=document.getElementsByName("COMPLAINT_NATURE_ID");
+for (var i=0;i<els.length;i++) {
+els[i].value = "12";}
+
+var els=document.getElementsByName("COMPLAINT_INDUSTRY_ID");
+for (var i=0;i<els.length;i++) {
+els[i].value = "11";}
+
+var els=document.getElementsByName("COMPLAINT_TYPE_ID");
+for (var i=0;i<els.length;i++) {
+els[i].value = "1";}
+
+var els=document.getElementsByName("COMPLAINT_TRANSACTION_DAY");
+for (var i=0;i<els.length;i++) {
+els[i].value = "15";}
+
+var els=document.getElementsByName("COMPLAINT_TRANSACTION_MONTH");
+for (var i=0;i<els.length;i++) {
+els[i].value = "2";}
+
+var els=document.getElementsByName("COMPLAINT_TRANSACTION_YEAR");
+for (var i=0;i<els.length;i++) {
+els[i].value = "2020";}
+
+var els=document.getElementsByName("COMPLAINT_OUTCOME_ID");
+for (var i=0;i<els.length;i++) {
+els[i].value = "1";}
+
+var els=document.getElementsByName("COMPLAINT_CONTRACT_VALUE");
+for (var i=0;i<els.length;i++) {
+els[i].value = "4";}
+
+var els=document.getElementsByName("COMPLAINT_QUANTUM_VALUE");
+for (var i=0;i<els.length;i++) {
+els[i].value = "0";}
+
+var els=document.getElementsByName("COMPLAINT_ONLINE");
+for (var i=0;i<els.length;i++) {
+els[i].value = "1";}
+
+var els=document.getElementsByName("COMPLAINT_SUMMARY");
+for (var i=0;i<els.length;i++) {
+els[i].value = "Item ordered: 7 days ago. Item arrived: 3 days ago. [Item is faulty and vendor is not responding to customer's request for a replacement.]";}
--- popup.js
+++ popup.js
@@ -1,41 +1,82 @@
   
+function autofill() {
+
+  chrome.tabs.executeScript({
+      file: 'autofill.js'
+  });
+
+}
+
+function openCase() {
+
+  window.open("https://app.case.org.sg/complaint.plx", "_blank"); 
+
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   
+          var button = document.getElementById('generate');
+          button.addEventListener('click', function () {
+              autofill();
+          });
+
+          var button = document.getElementById('openCase');
+          button.addEventListener('click', function () {
+              openCase();
+          });
 
   chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
     function(tabs){
+
 
       var currUrl = tabs[0].url;
 
       if(currUrl == "https://shopee.sg/") {
         document.body.style.backgroundColor = "orange";
-
-          // chrome.browserAction.setPopup({
-          //    popup:"shopee.html"
-          // });
       
           document.getElementById("lazada").setAttribute("hidden","");
           document.getElementById("shopee").removeAttribute("hidden");
-          document.getElementById("here").src = "shopee.html";
+          
+          //document.getElementById("dynamicContent").src = "shopee.html";
+
+          
+          
+
+          //autofill();
+
+          // $(function(){
+          //   $("#dynamicContent").load("shopee.html"); 
+          // });
+          
+
       } else {
+      
 
 
-      //   document.body.style.backgroundColor = "white";
-      //   chrome.browserAction.setPopup({
-      //     popup:"popup.html"
-      //  });
+        document.getElementById("lazada").removeAttribute("hidden");
+        document.getElementById("shopee").setAttribute("hidden","");
 
 
-          document.getElementById("lazada").removeAttribute("hidden");
-          document.getElementById("shopee").setAttribute("hidden","");
+        // if(confirm("do you want to launch case complaint?")){
+        //   if(currUrl == "https://app.case.org.sg/complaint.plx") {
+        //   autofill();
+        // } else {
+        //   openCase();
+        // }
 
-       }
+        // }
+
+        
+          
+      
+
+       } 
       
 
 
     }
   );
-
   
 
 }, false)
+
